e8208de54a70f798751c5c440d052fc9c48fb4c1
pezhiyi <pengzhiyi9910@gmail.com>
2025-01-16 00:23:20 +0800

    test

--- app_page.tsx
+++ app_page.tsx
@@ -1,4 +1,4 @@
-import LogoAnimation from '../components/LogoAnimation'
+import LogoAnimation from './components/LogoAnimation'
 
 export default function Home() {
   return (
@@ -7,4 +7,3 @@ export default function Home() {
     </main>
   )
 }
-
